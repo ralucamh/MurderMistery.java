2e0e2a9568dbd3d68b2cb434db327ea19541caf1
IlincaT04 <147716876+IlincaT04@users.noreply.github.com>
2023-10-17 16:53:50 +0200

    Table Moved

--- Display.java
+++ Display.java
@@ -43,25 +43,27 @@ public class Display {
         String[] columnNames = { "Who?", "", "What?", "", "Where?", "" };
         
         JTable j = new JTable(data, columnNames);
-        j.setBounds(50, 100, 100, 100);
-        j.setVisible(true);
-    
-        JScrollPane sp = new JScrollPane(j);
-        Panel.add(sp, BorderLayout.CENTER);
-    
-        //Panel.setLocation(250, 170);
-      //  sp.setLocation(250, 75);
+  
+
+        j.setRowHeight(30);
+         JScrollPane sp = new JScrollPane(j);
+         Global.frame.add(sp);
+        sp.setSize(700,230);
+        sp.setVisible(true);
+
+        sp.setLocation(0, 300);
+        
 
         JButton button = new JButton("Roll the dice");
         button.setBackground(Color.green);
-        button.setBounds(150, 100, 20, 40);
-        // button.setPreferredSize(new Dimension(100, 30));
-        Panel.add(button, BorderLayout.SOUTH);
+        button.setBounds(265, 570, 170, 50);
+        Global.frame.add(button);
+
         MyListener myListener = new MyListener();
         button.addActionListener(myListener);
 
         Global.frame.add(Panel);
-        Global.frame.setSize(500, 500);
+        Global.frame.setSize(700, 700);
         Global.frame.setVisible(true);
         Global.frame.setLocationRelativeTo(null);
         Global.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
