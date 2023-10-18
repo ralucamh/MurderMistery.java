202f6e2476360af74d5f491e035b24b6580f4eff
IlincaT04 <147716876+IlincaT04@users.noreply.github.com>
2023-10-18 21:05:58 +0200

    Update Display.java

--- Display.java
+++ Display.java
@@ -71,6 +71,49 @@ public class Display {
         
 
     }
+    void makeassumption(){
+        
+    }
+    void players(String[] player){
+       String v[];
+       int k = 0;
+       Random r = new Random();
+       int n;
+       
+       for(int i = 0; i < 4; i++){
+          if (user[i].equals(assumption[0]) || user[i].equals(assumption[1]) || user[i].equals(assumption[2])){
+            v[++k] = user[i]; 
+          }
+       }
+
+       for(int i = 0; i < 4; i++){
+          if (player2[i].equals(assumption[0]) || player2[i].equals(assumption[1]) || player2[i].equals(assumption[2])){
+            v[++k] = player2[i]; 
+          }
+       }
+
+       for(int i = 0; i < 4; i++){
+          if (player3[i].equals(assumption[0]) || player3[i].equals(assumption[1]) || player3[i].equals(assumption[2])){
+            v[++k] = player3[i]; 
+          }
+       }
+
+       for(int i = 0; i < 4; i++){
+          if (player4[i].equals(assumption[0]) || player4[i].equals(assumption[1]) || player4[i].equals(assumption[2])){
+            v[++k] = player4[i]; 
+          }
+       }
+
+       int k1 = k;
+       while (k1 > 0){
+         n = r.nextInt(k);
+         for(int j = 0;j < 19; j++){
+            if(checklist[j].equals(v[k]) && !checked(v[k])){
+                checked(mychecklist) = true;
+            }
+         }
+       }
+    }
 
     public static void main(String[] args) {
         new Display().create();
