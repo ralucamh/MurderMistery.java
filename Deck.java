d2ff0479e3d2660605f4a1453c5aef0953ab0a1f
ralucamh <147708278+ralucamh@users.noreply.github.com>
2023-10-19 10:52:42 +0200

    Start Deck

---
+++ Deck.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Deck {
+    
+    String[] deck = new String[16];
+    String[] user = new String[4];
+    String[] player1 = new String[4];
+    String[] player2 = new String[4];
+    String[] player3 = new String[4];
+
+    deck[0] = "Marcela";
+    
+
+}
