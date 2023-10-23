acf3c2ba3162a636745c6044564801dbb80e6191
IlincaT04 <147716876+IlincaT04@users.noreply.github.com>
2023-10-23 12:25:52 +0200

    almost done but not really

--- Algorithm.java
+++ Algorithm.java
@@ -1,64 +1,225 @@
 import java.util.Random;
 
 public class Algorithm {
-    void makeassumption(){
-        
+   String[] assumption;
+   String ask;
+   
+   void asksuspect(boolean[] checklist, int suspicion[][]) {
+      
+      int mx = 0;
+      Random r = new Random();
+      int n;
+    for (int i = 0; i < 6; i ++) {
+         for(int j = 0; j < 4; j ++){
+         if (suspicion[j][i] > mx && checklist[i] == false) {
+            ask = deck[i];
+            mx = suspicion[j][i];
+         }
+      }
+      }
+
+      while (ask == null) {
+      n = r.nextInt(6);
+      if (checklist[n] == false) {
+      ask = deck[n];
+      }
+   }
+   }
+
+   void askweapon(boolean[] checklist, int suspicion[][]) {
+      
+      int mx = 0;
+      Random r = new Random();
+      int n;
+    for (int i = 6; i < 12; i ++) {
+         for(int j = 0; j < 4; j ++){
+         if (suspicion[j][i] > mx && checklist[i] == false) {
+            ask = deck[i];
+            mx = suspicion[j][i];
+         }
+      }
+      }
+
+      while (ask == null) {
+      n = 6 + r.nextInt(6);
+      if (checklist[n] == false) {
+      ask = deck[n];
+      }
+   }
+   }
+
+   void askroom(boolean[] checklist, int suspicion[][]) {
+      
+      int mx = 0;
+      Random r = new Random();
+      int n;
+    
+      for (int i = 12; i < 19; i ++) {
+         for(int j = 0; j < 4; j ++){
+         if (suspicion[j][i] > mx && checklist[i] == false) {
+            ask = deck[i];
+            mx = suspicion[j][i];
+         }
+      }
+      }
+
+      while (ask == null) {
+      n = 12 + r.nextInt(6);
+      if (checklist[n] == false) {
+      ask = deck[n];
+      }
+   }
+   }
+
+    void makeassumption(boolean[] checklist, int suspicion[][]){
+      
+      int mx = 0;
+      Random r = new Random();
+      int n;
+      
+      for (int i = 0; i < 6; i ++) {
+         for(int j = 0; j < 4; j ++){
+         if (suspicion[j][i] > mx && checklist[i] == false) {
+            assumption[0] = deck[i];
+            mx = suspicion[j][i];
+         }
+      }
+      }
+
+      mx = 0;
+
+       for (int i = 6; i < 12; i ++) {
+         for(int j = 0; j < 4; j ++){
+         if (suspicion[j][i] > mx && checklist[i] == false) {
+            assumption[1] = deck[i];
+            mx = suspicion[j][i];
+         }
+      }
+      }
+      
+      mx = 0;
+
+      for (int i = 12; i < 19; i ++) {
+         for(int j = 0; j < 4; j ++){
+         if (suspicion[j][i] > mx && checklist[i] == false) {
+            assumption[2] = deck[i];
+            mx = suspicion[j][i];
+         }
+      }
+      }
+
+   while (assumption[0] == null) {
+      n = r.nextInt(6);
+      if (checklist[n] == false) {
+      assumption[0] = deck[n];
+      }
+   }
+
+   while (assumption[1] == null) {
+      n = 6 + r.nextInt(6);
+      if (checklist[n] == false) {
+      assumption[1] = deck[n];
+      }
+   }
+
+   while (assumption[2] == null) {
+      n = 12 + r.nextInt(6);
+      if (checklist[n] == false) {
+      assumption[2] = deck[n];
+      }
+   }
     }
     
-    void turn(String[] player){
-       String v[];
+    void turn(String[][] player, boolean[] checklist){
+      int suspicion[][]; 
+      String v[];
+      int w[];
        String question;
        int k = 0;
        Random r = new Random();
        int n;
-       
+       for (int i = 0; i < 4; i ++){
+       for(int j = 0; j < 19; j ++) {
+         suspicion[i][j] = 0; 
+       }
+      }
        for(int i = 0; i < 4; i++){
-          if (user[i].equals(assumption[0]) || user[i].equals(assumption[1]) || user[i].equals(assumption[2])){
-            v[++k] = user[i]; 
+          if (player[0][i].equals(assumption[0]) || player[0][i].equals(assumption[1]) || player[0][i].equals(assumption[2])){
+            v[++k] = player[0][i]; 
+            w[k] = 0;
           }
        }
 
        for(int i = 0; i < 4; i++){
-          if (player2[i].equals(assumption[0]) || player2[i].equals(assumption[1]) || player2[i].equals(assumption[2])){
-            v[++k] = player2[i]; 
+          if (player[1][i].equals(assumption[0]) || player[1][i].equals(assumption[1]) || player[1][i].equals(assumption[2])){
+            v[++k] = player[1][i]; 
+            w[k] = 1;
           }
        }
 
        for(int i = 0; i < 4; i++){
-          if (player3[i].equals(assumption[0]) || player3[i].equals(assumption[1]) || player3[i].equals(assumption[2])){
-            v[++k] = player3[i]; 
+          if (player[2][i].equals(assumption[0]) || player[2][i].equals(assumption[1]) || player[2][i].equals(assumption[2])){
+            v[++k] = player[2][i]; 
+            w[k] = 2;
           }
        }
 
        for(int i = 0; i < 4; i++){
-          if (player4[i].equals(assumption[0]) || player4[i].equals(assumption[1]) || player4[i].equals(assumption[2])){
-            v[++k] = player4[i]; 
+          if (player[3][i].equals(assumption[0]) || player[3][i].equals(assumption[1]) || player[3][i].equals(assumption[2])){
+            v[++k] = player[3][i]; 
+            w[k] = 3;
           }
        }
        boolean ok;
+       int nr = 0;
         while(ok == false) {
          n = r.nextInt(k);
-         for(int j = 0;j < 19; j++){
-            if (checklist[j].equals(v[n]) && ! player.checked(v[n])){
-                player.checked(v[n]) = true;
+         for(int i = 0;i < 19; i ++){
+            if (deck[i].equals(v[n]) && checklist[i] == false){
+                checklist[i] = true;
                 ok = true;
-                question = v[n];
+                for (int j = 0; j < k; j ++) {
+                suspicion[w[n]][j] ++;
+                }
             }
          }
-         v[n] = null;
-         for(int i = 0; i < k; i++) {
-            if ( v[i] != NULL){
-                ok1 = false;
-            }
+         if (ok == false) {
+            nr ++;
          }
-         boolean ok1;
-         if (ok1 == true) {
+         
+         if (nr == k) {
             ok = true;
          }
         }
        
-       for (int i = 0; i < 19; i ++) {
-        
-       } 
+       if (nr == k) {
+         System.out.println("");
+       }
+
     }
+
+    void turn1 () {
+      boolean ok;
+      int x;
+      for (int i = 0; i < 19; i ++) {
+         if (deck[i].equals(ask)) {
+            x = i;
+         }
+      }
+      for (int i = 0; i < 4; i ++) {
+      if (player[n][i].equals(ask)) {
+         ok = true;
+         myChecklist[x] = true;
+         checklist2[x] = true;
+         checklist3[x] = true;
+         checklist4[x] = true;
+      }
+    }
+
+    if (ok == false) {
+      for(int i = 0; i < 4; i ++) {
+         suspicion[i][x] = -5;
+      }
+    }
+}
 }
