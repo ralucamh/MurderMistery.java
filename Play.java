5331405c9ffecbb18f0114f9d093361ed0d47f73
IlincaT04 <147716876+IlincaT04@users.noreply.github.com>
2023-10-29 16:38:58 +0100

    changes

--- Play.java
+++ Play.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import java.util.*;
+
 public class Play {
     public static int[][] suspicion2 = new int[4][19];
     public static int[][] suspicion3 = new int[4][19];
@@ -7,26 +10,28 @@ public class Play {
     public static boolean[] checklist3 = new boolean[19];
     public static boolean[] checklist4 = new boolean[19];
     public static int winner = -1;
+
+    Algorithm alg = new Algorithm();
     void game() {
         while (winner == -1) {
         if (dice == 5 || dice == 6) {
         // assumption
-        Algorithm.turn (player[0], myChecklist, 0);
-        makeassumption (checklist2, suspicion2);
-        turn (player[1], checklist2, 1);
-        makeassumption (checklist3, suspicion3);
-        turn (player[2], checklist3, 2);
-        makeassumption (checklist4, suspicion4);
-        turn (player[3], checklist4, 3);
-    } else
+        alg.turns(myChecklist, 0);
+        alg.makeassumption (checklist2, suspicion2);
+        alg.turns (checklist2, 1);
+        alg.makeassumption (checklist3, suspicion3);
+        alg.turn (player[2], checklist3, 2);
+        alg.makeassumption (checklist4, suspicion4);
+        alg.turn (player[3], checklist4, 3);
+    } elalg
       if (dice == 3 || dice == 4) {
-        turn (player[0]);
-        makeassumption (checklist2, suspicion2);
-        turn (player[1]);
-        makeassumption (checklist3, suspicion3);
-        turn (player[2]);
-        makeassumption (checklist4, suspicion4);
-        turn (player[3]);
+        alg.turn1 (player[0]);
+        alg.makeassumption (checklist2, suspicion2);
+        alg.turn1 (player[1]);
+        alg.makeassumption (checklist3, suspicion3);
+        alg.turn1 (player[2]);
+        alg.makeassumption (checklist4, suspicion4);
+        alg.turn1 (Deck.players[3]);
       }
 
     }
